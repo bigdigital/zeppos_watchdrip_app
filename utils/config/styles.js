6724b2d68fddd087fc8b753cfd5bc40af5ca930f
bigdigital <bigdigital.kov@gmail.com>
2023-06-25 21:42:25 +0300

    add mi-band7 support

--- utils/config/styles.js
+++ utils/config/styles.js
@@ -7,25 +7,47 @@ export const DEVICE_TYPE = DEVICE_SHAPE ? 'round' : 'square'
 
 const STATUS_BAR_HEIGHT = 20;
 var TEXT_SIZE = px(36);
-if (DEVICE_WIDTH < 340){
-  TEXT_SIZE = px(20)
-}
+var TEXT_HEIGHT = px(42);
+var BG_TIME_TEXT_X = DEVICE_WIDTH/2 + px(90);
+var BG_TIME_TEXT_Y = px(126);
+var BG_TIME_TEXT_WIDTH = px(200);
+var BG_TIME_TEXT_ALIGN_H = hmUI.align.LEFT;
 
+var SCROLL_ITEM_HEIGHT = px(90);
 var COMMON_BUTTON_PADDING = 0;
 var SCROLL_ITEM_PADDING = 0;
 export const MESSAGE_TEXT_SIZE = px(38);
 export var MESSAGE_TEXT_WIDTH = DEVICE_WIDTH;
 export var RADIO_ON = "radio_on_rect.png";
 export var RADIO_OFF = "radio_off_rect.png";
-var SCROL_PAGE_Y = STATUS_BAR_HEIGHT
 
-if (DEVICE_TYPE === "round"){
-  SCROL_PAGE_Y = px(90);
+var STATE_IMG_WIDTH = 80;
+var STATE_IMG_HEIGHT = 60;
+
+var SCROLL_PAGE_Y = STATUS_BAR_HEIGHT
+
+if (DEVICE_WIDTH < 340){
+    TEXT_SIZE = px(26);
+    TEXT_HEIGHT = px(36);
+    BG_TIME_TEXT_X = (DEVICE_WIDTH - BG_TIME_TEXT_WIDTH) / 2;
+    BG_TIME_TEXT_Y = px(280);
+    BG_TIME_TEXT_ALIGN_H = hmUI.align.CENTER_H;
+    RADIO_ON = "radio_on_tiny.png";
+    RADIO_OFF = "radio_off_tiny.png";
+    SCROLL_ITEM_HEIGHT = px(70);
+    STATE_IMG_WIDTH = px(24);
+    STATE_IMG_HEIGHT = px(24);
+}
+else if (DEVICE_TYPE === "round"){
+  SCROLL_PAGE_Y = px(90);
   COMMON_BUTTON_PADDING = px(50);
   SCROLL_ITEM_PADDING = px(30);
   RADIO_ON = "radio_on.png";
   RADIO_OFF = "radio_off.png";
   MESSAGE_TEXT_WIDTH = DEVICE_WIDTH - px(20);
+
+  STATE_IMG_WIDTH = 100;
+  STATE_IMG_HEIGHT = 68;
 }
 
 export const DEBUG_TEXT = {
@@ -46,7 +68,7 @@ export const TITLE_TEXT = {
   x: (DEVICE_WIDTH - px(288)) / 2,
   y: px(15),
   w: px(288),
-  h: px(42),
+  h: TEXT_HEIGHT,
   color: Colors.white,
   text_size: TEXT_SIZE,
   align_h: hmUI.align.CENTER_H,
@@ -103,7 +125,7 @@ export const COMMON_BUTTON_ADD_TREATMENT = {
 
 export const VERSION_TEXT = {
   x: (DEVICE_WIDTH - px(288)) / 2,
-  y: DEVICE_HEIGHT + px(135),
+  y: DEVICE_HEIGHT + px(130),
   w: px(288),
   h: px(28),
   color: Colors.white,
@@ -126,13 +148,13 @@ export const BG_VALUE_TEXT = {
 };
 
 export const BG_TIME_TEXT = {
-  x: DEVICE_WIDTH/2 + px(90),
-  y: px(126),
-  w: px(200),
+  x: BG_TIME_TEXT_X,
+  y: BG_TIME_TEXT_Y,
+  w: BG_TIME_TEXT_WIDTH,
   h: px(32),
   color: Colors.white,
   text_size: px(25),
-  align_h: hmUI.align.LEFT,
+  align_h: BG_TIME_TEXT_ALIGN_H,
   align_v: hmUI.align.CENTER_V,
   text_style: hmUI.text_style.NONE,
 };
@@ -141,9 +163,9 @@ export const BG_DELTA_TEXT = {
   x: (DEVICE_WIDTH - px(200)) / 2,
   y: px(230),
   w: px(200),
-  h: TEXT_SIZE,
+  h: TEXT_HEIGHT,
   color: Colors.white,
-  text_size: px(28),
+  text_size: TEXT_SIZE,
   align_h: hmUI.align.CENTER_H,
   align_v: hmUI.align.CENTER_V,
   text_style: hmUI.text_style.NONE,
@@ -178,10 +200,6 @@ export const IMG_LOADING_PROGRESS = {
 
 
 
-const STATE_IMG_WIDTH = 100;
-const STATE_IMG_HEIGHT = 64;
-const SCROLL_ITEM_HEIGHT = px(90);
-
 export const CONFIG_PAGE_SCROLL_ITEM_CONFIG = [
   {
     type_id: 1,
@@ -215,7 +233,7 @@ export const CONFIG_PAGE_SCROLL_ITEM_CONFIG = [
 
 export const CONFIG_PAGE_SCROLL = {
   x: SCROLL_ITEM_PADDING,
-  y: SCROL_PAGE_Y,
+  y: SCROLL_PAGE_Y,
   h: DEVICE_HEIGHT - px(20),
   w: DEVICE_WIDTH - SCROLL_ITEM_PADDING*2,
   item_space: px(10),
